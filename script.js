44778b4c8a211fa748bbc93badc8d8ff6e775494
Zyanvel <zyanvel@gmail.com>
2020-10-11 00:26:44 -0700

    First commit, yay

---
+++ script.js
@@ -0,0 +1,5 @@
+let userName = prompt("Hello! What is your name?");
+let userLastName = prompt("...and your last name?");
+let userAge = prompt("That is a cute name! Finally, how old are you?");
+console.log(`Nice to meet you, ${userName} ${userLastName}! :)`);
+console.log(`You told me you are ${userAge} years old, that's cool! :)`);
